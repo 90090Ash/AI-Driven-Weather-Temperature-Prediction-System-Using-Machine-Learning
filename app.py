a46e3e14957095cf4e2ac244eb82ee188926513d
90090Ash <53510298+90090Ash@users.noreply.github.com>
2026-02-04 15:13:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 model = pickle.load(open("model/weather_model.pkl", "rb"))
 
@@ -27,3 +29,4 @@ def predict():
 
 if __name__ == "__main__":
     app.run()
+
